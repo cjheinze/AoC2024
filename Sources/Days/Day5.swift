0baf5597464c3cce2c8afa42af5dcc871c790ed2
Carl-Johan Heinze <cj.heinze@mac.com>
2024-12-05 14:33:00 +0100

    Cleanup day5

--- Sources/Days/Day5.swift
+++ Sources/Days/Day5.swift
@@ -6,7 +6,7 @@ import Algorithms
 public class Day5: DayProtocol {
     private let input: String
     private let rules: [String]
-    private let orders: [String]
+    private let orders: [[Int]]
     
     private let illegalSubsequentPages: [Int: [Int]]
     
@@ -14,50 +14,47 @@ public class Day5: DayProtocol {
         input = FileHandler.getInputs(for: Bundle.module.resourcePath!, andDay: 5)
         
         let sections = input.split(separator: "\n\n").map(String.init)
-        rules = sections[0].split(separator: "\n").map(String.init)
+        rules = sections[0] .split(separator: "\n").map(String.init)
         orders = sections[1].split(separator: "\n").map(String.init)
-        illegalSubsequentPages = rules.map { rule in
-            let pair = rule.split(separator: "|").map(String.init)
-            return (x: Int(pair[0])!, y: Int(pair[1])!)
-        }.reduce(into: [Int: [Int]]()) { map, rule in
-            map[rule.y, default: [Int]()].append(rule.x)
-        }
+            .map { $0.split(separator: ",").map { Int($0)! } }
+        illegalSubsequentPages = rules
+            .map { rule in
+                let pair = rule.split(separator: "|").map(String.init)
+                return (x: Int(pair.first!)!, y: Int(pair.last!)!)
+            }.reduce(into: [Int: [Int]]()) { map, rule in
+                map[rule.y, default: [Int]()].append(rule.x)
+            }
     }
     
     public func partOne() -> String {
         
-        return orders.compactMap { order in
-            let pages = order.split(separator: ",").map({ Int ($0)! })
-            let isValid = pageOrderCorrect(order: order)
-            return isValid ? pages[pages.count / 2] : nil
+        return orders.compactMap { pages in
+            pageOrderCorrect(pages: pages) ? pages.getMiddleValue() : nil
         }.reduce(0, +).description
     }
     
     public func partTwo() -> String {
-        var enumeratedOrders = orders.enumerated()
-        var checkedOrders = enumeratedOrders.compactMap { index, order in
-            let pages = order.split(separator: ",").map({ Int ($0)! })
-            let isValid = pageOrderCorrect(order: order)
-            return (index, order, isValid)
-        }
-        
-        return checkedOrders.filter({ !$0.2 })
-            .map { index, order, _ in
-                let pages = order.split(separator: ",").map({ Int ($0)! })
-                let fixed = pages.sorted { a, b in
-                    illegalSubsequentPages[a]?.contains(b) ?? true
-                }
-                return fixed[fixed.count / 2]
-            }.reduce(0, +).description
+        return orders.compactMap { pages in
+            pageOrderCorrect(pages: pages) ? nil : pages.sorted { a, b in
+                illegalSubsequentPages[a]?.contains(b) ?? true
+            }.getMiddleValue()
+        }.reduce(0, +).description
     }
     
-    
-    fileprivate func pageOrderCorrect(order: String) -> Bool {
-        let pages = order.split(separator: ",").map({ Int ($0)! })
-        return pages.enumerated().map { index, page in
+    fileprivate func pageOrderCorrect(pages: [Int]) -> Bool {
+        pages.enumerated().map { index, page in
             let nextPages = pages.dropFirst(index + 1)
             return illegalSubsequentPages[page]?.allSatisfy { !nextPages.contains($0) } ?? true
         }.allSatisfy { $0 }
     }
 
+}
+
+extension Array where Element == Int {
+    func getMiddleValue() -> Int {
+        guard count > 0 else {
+            fatalError("Array is empty")
+        }
+        return self[self.count / 2]
+    }
 }
